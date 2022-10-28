31220f7ddb3257719e10dd19fae25d36f472ded9
Elias Kirui <eliaskips1@gmail.com>
2022-10-28 13:04:45 +0300

    Change functions to classes and handling states

--- app/javascript/react/src/components/Welcome.jsx
+++ app/javascript/react/src/components/Welcome.jsx
@@ -1,16 +1,23 @@
 import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import * as ReactDOM from 'react-dom/client'
+import QuestionList from "./QuestionList";
 
-const Welcome = () => {
-    return(
-        <div className="container">
-            <h1>Welcome to Westwood Tech School!</h1>
-            <p className="lead">Are you passionate about tech? Then you have come to the right place!</p>
-        </div>
-    )
+class Welcome extends React.Component {
+    render() {
+        return(
+            <div className="container">
+                <h1>Welcome to Westwood Tech School!</h1>
+                <p className="lead">Are you passionate about tech? Then you have come to the right place!</p>
+                <QuestionList />
+            </div>
+        )
+    }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    ReactDOM.render(< Welcome/>, document.getElementById('welcome'))
-})
+const root = ReactDOM.createRoot(document.getElementById('welcome'))
+root.render(
+    <React.StrictMode>
+        <Welcome />
+    </React.StrictMode>
+)
 export default Welcome
