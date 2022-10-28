8590d35bf99b45c127c80ea0c25ddcce743960eb
Elias Kirui <eliaskips1@gmail.com>
2022-10-28 13:36:04 +0300

    Create api endpoints to fetch data from react components

--- app/javascript/react/src/components/QuestionList.jsx
+++ app/javascript/react/src/components/QuestionList.jsx
@@ -1,66 +1,26 @@
 import * as React from 'react'
+import { useState, useEffect } from "react";
 import * as ReactDOM from 'react-dom'
 import QuestionDetail from "./QuestionDetail";
 
 const QuestionList = () => {
-    const questionList = [
-        {
-            id: 1,
-            title: 'How to check if a key is present in a Hash?',
-            tag: 'Ruby'
-        },
-        {
-            id: 2,
-            title: 'What is the difference between a string and symbol?',
-            tag: 'Ruby'
-        },
-        {
-            id: 3,
-            title: 'What happens if you add two same keys in a Hash?',
-            tag: 'Ruby'
-        },
-        {
-            id: 4,
-            title: 'How to delete a given jey from hash?',
-            tag: 'Ruby'
-        },
-        {
-            id: 5,
-            title: 'How to check if two hashes are identical?',
-            tag: 'Ruby'
-        },
-        {
-            id: 6,
-            title: 'What does the has_key?, key? member? and include? methods in a hash?',
-            tag: 'Ruby'
-        },
-        {
-            id: 7,
-            title: 'How to get unique keys from two hashes in Ruby?',
-            tag: 'Ruby'
-        },
-        {
-            id: 8,
-            title: 'What are blocks in Ruby?',
-            tag: 'Ruby'
-        },
-        {
-            id: 9,
-            title: 'How to check if two hashes are identical?',
-            tag: 'Ruby'
-        },
-        {
-            id: 10,
-            title: 'Does the order of keys matter ito compare two hashes in Ruby?',
-            tag: 'Ruby'
-        }
-
-    ]
-
+    const [questionsList, setQuestionsList] = useState([])
+    const questionsUrl = 'http://localhost:3000/api/v1/questions'
+    const fetchQuestionList = () => {
+        fetch(questionsUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                setQuestionsList(data)
+            })
+    }
+    useEffect(() => {
+        fetchQuestionList()
+    }, [])
     return(
         <div className="row">
             <div className="col-lg-10 mx-auto">
-                {questionList.map((question) =>
+                {questionsList.map((question) =>
                         <QuestionDetail question={question} key={question.id}/>
                 )}
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,82 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Creating questions'
+questions = Question.create ([
+  {
+    title: 'How to check if a key is present in a Hash?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'What is the difference between a string and symbol?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'What happens if you add two same keys in a Hash?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'How to delete a given key from hash?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'How to check if two hashes are identical?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'What does the has_key?, key? member? and include? methods in a hash?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'How to get unique keys from two hashes in Ruby?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'What are blocks in Ruby?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'How to check if two hashes are identical?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'Does the order of keys matter ito compare two hashes in Ruby?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'What is render method in react class component?',
+    tag: 'React'
+  },
+  {
+    title: 'What is the difference between class and functional components in React?',
+    tag: 'React'
+  },
+  {
+    title: 'What is the key property in React?',
+    tag: 'React'
+  },
+  {
+    title: 'What is render method in react class component?',
+    tag: 'React'
+  },
+  {
+    title: 'How to get unique keys from two hashes in Ruby?',
+    tag: 'React'
+  },
+  {
+    title: 'What are hooks in React?',
+    tag: 'React'
+  },
+  {
+    title: 'What is useState in React?',
+    tag: 'React'
+  },
+  {
+    title: 'What is creatorRoot method in React?',
+    tag: 'React'
+  }
+
+])
+
+puts 'Questions created!'
